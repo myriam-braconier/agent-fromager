0f19999ada2c8ae4317f5bc5764152e02fb3d358
Volubyl <myriam.braconier@gmail.com>
2026-02-10 16:36:36 +0100

    fix: interface.launch

--- app.py
+++ app.py
@@ -9059,7 +9059,7 @@ if __name__ == "__main__":
 
     if interface:
         interface.launch(
-        share=True,
+        share=False,
         server_name="0.0.0.0",
         server_port=7860,
         debug=False,
